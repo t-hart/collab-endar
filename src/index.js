2295e206696f06fcf557fede2ccb00e71be12e20
Timothy Hart <thart31@gatech.edu>
2024-12-04 20:05:29 -0500

    Revert "add basic style, add new page"
    This reverts commit 7687d9616dae41ebffe4ba2e326cbe9fd108fcd5.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import Planner from './Planner';
 
-ReactDOM.render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/planner" element={<Planner />} />
-    </Routes>
-  </Router>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
