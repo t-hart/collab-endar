324cf13b0889839ab2efb227e0e24b4a9f5f55a5
Timothy Hart <thart31@gatech.edu>
2024-12-06 05:50:47 -0500

    implement signalR event listeners

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
+import { HubConnectionBuilder, LogLevel, HubConnection } from '@microsoft/signalr';
 import { DateCard } from "./components/DateCard"
 import { v4 as uuid } from 'uuid';
 import { Stack } from '@mui/material';
@@ -55,6 +55,82 @@ function App() {
 
     })
   };
+
+  const [connection, setConnection] = useState<HubConnection | null>(null);
+  useEffect(() => {
+    const startSignalRConnection = async () => {
+      try {
+        // placeholder for now
+        const planId = "plan_ee583bd1";
+
+        // First, negotiate with the Azure Function
+        const response = await fetch(`/api/negotiate`);
+        if (!response.ok) {
+          throw new Error(`Negotiation failed: ${response.statusText}`);
+        }
+        const connectionInfo = await response.json();
+
+        const connection = new HubConnectionBuilder()
+          .withUrl(connectionInfo.url, { accessTokenFactory: () => connectionInfo.accessToken })
+          .withAutomaticReconnect()
+          .build();
+
+        // Add event listeners
+        connection.on("planCreated", (plan) => {
+          console.log("[SignalR] planCreated: ", plan);
+        });
+
+        connection.on("planDelete", (id) => {
+          console.log("[SignalR] planDeleted: ", id);
+        });
+
+        connection.on("dateAdded", (date) => {
+          console.log("[SignalR] dateAdded: ", date);
+        });
+
+        connection.on("dateDeleted", (id) => {
+          console.log("[SignalR] dateDeleted: ", id);
+        });
+
+        connection.on("activityAdded", (activity) => {
+          console.log("[SignalR] activityAdded: ", activity);
+        });
+
+        connection.on("activityDeleted", (id) => {
+          console.log("[SignalR] activityDeleted: ", id);
+        });
+
+        connection.on("lockActivity", (id) => {
+          console.log("[SignalR] lockActivity: ", id);
+        });
+
+        connection.on("activityUpdated", (update) => {
+          console.log("[SignalR] activityUpdated: ", update);
+        });
+
+        // Start signalR connection
+        await connection.start();
+        console.log("Connected to SignalR hub");
+
+        // Register user
+        const registerUser = await fetch(`/api/registerUser?planId=${planId}&connectionId=${connection.connectionId}`);
+        if (!registerUser.ok) {
+          throw new Error(`Registering user failed: ${registerUser.statusText}`);
+        }
+        setConnection(connection);
+      } catch (err) {
+        console.error("SignalR Connection Error: ", err);
+      }
+    };
+    startSignalRConnection();
+    
+    // Cleanup on unmount
+    return () => {
+      if (connection) {
+        connection.stop();
+      }
+    };
+  }, []);
 
   return (
     <Stack direction="row" spacing={2}>
